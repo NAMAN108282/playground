0facc7842be28d0c7671f7a41dac6bc365b74160
Naman <49680172+NAMAN108282@users.noreply.github.com>
2023-06-11 18:35:58 +0530

    Add files via upload

---
+++ jpegCheck.py
@@ -0,0 +1,23 @@
+import sys
+
+def checkFile():
+    if len(sys.argv) < 1:
+        print("File must be specified")
+        return
+
+    try:
+        rp = open(sys.argv[1], 'rb')
+    except FileNotFoundError:
+        print("File not found")
+        return
+
+    if rp == None:
+        print("File not readable")
+        return
+    if rp.read(3) == b'\xff\xd8\xff':
+        print("Yes this is an image file")
+    else:
+        print("Not an image file")
+
+if __name__ == "__main__":
+    checkFile()
